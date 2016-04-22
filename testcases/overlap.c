75da2d108f5f608007015bb73048c5527fdf736a
huangtw <huangtw0422@gmail.com>
2016-04-21 23:09:26 -0400

    add new testcases and change function names in testcases

--- testcases/overlap.c
+++ testcases/overlap.c
@@ -7,31 +7,30 @@
 #include <assert.h>
 #include <sys/wait.h>
 
-
 void proc1() 
 {
+
      rvm_t rvm = rvm_init("rvm_segments");
      rvm_destroy(rvm, "testseg1");
-     rvm_destroy(rvm, "testseg2");
-     char* segs[2];
+     char* segs[1];
      segs[0] = (char *) rvm_map(rvm, "testseg1", 6);
-     segs[1] = (char *) rvm_map(rvm, "testseg2", 6);
 
      trans_t tid;
 
-     tid = rvm_begin_trans(rvm, 2, (void **) segs);
-     rvm_about_to_modify(tid, segs[0], 0, 5);
-     sprintf(segs[0], "    ");
-     rvm_commit_trans(tid);
+     tid = rvm_begin_trans(rvm, 1, (void **) segs);
+     rvm_about_to_modify(tid, segs[0], 0, 6);
+     sprintf(segs[0], "aaaaa");
+     rvm_about_to_modify(tid, segs[0], 3, 6);
+     sprintf(&segs[0][3], "bbbbb");
+     rvm_abort_trans(tid);
+     assert(strlen(segs[0]) == 0);
 
-     rvm_unmap(rvm, segs[1]);
 
-     segs[1] = (char *) rvm_map(rvm, "testseg2", 10);
-     tid = rvm_begin_trans(rvm, 2, (void **) segs);
-     rvm_about_to_modify(tid, segs[1], 0, 2);
-     sprintf(&(segs[1][0]), "a");
-     rvm_about_to_modify(tid, segs[1], 1, 2);
-     sprintf(&(segs[1][1]), "z");
+     tid = rvm_begin_trans(rvm, 1, (void **) segs);
+     rvm_about_to_modify(tid, segs[0], 0, 6);
+     sprintf(segs[0], "ccccc");
+     rvm_about_to_modify(tid, segs[0], 3, 6);
+     sprintf(&segs[0][3], "ddddd");
      rvm_commit_trans(tid);
 
      abort();
@@ -39,16 +38,13 @@ void proc1()
 
 void proc2() 
 {
-     char* segs[2];
+     char* segs[1];
      rvm_t rvm;
 
      rvm = rvm_init("rvm_segments");
 
      segs[0] = (char *) rvm_map(rvm, "testseg1", 10000);
-     assert(strcmp("    ", segs[0]) == 0);
-
-     segs[1] = (char *) rvm_map(rvm, "testseg2", 10000);
-     assert(strcmp("az", segs[1]) == 0);
+     assert(strcmp("cccddddd", segs[0]) == 0);
 
      printf("OK\n");
      exit(0);
